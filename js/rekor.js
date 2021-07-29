9665858deb8f7f3404a1c83b9b8d9bc59a88ca84
Forgotosleep <markbrian33@gmail.com>
2021-07-30 02:30:37 +0700

    Pretty sure it's version 0.85 at the very least. 8th commit.

--- js/rekor.js
+++ js/rekor.js
@@ -2,7 +2,9 @@
 let parent = localStorage.currParent
 let child = localStorage.currChild
 let data = JSON.parse(localStorage.getItem(parent))
-let counter = 1
+data[child].push(localStorage.currLevel)
+localStorage.setItem(parent, JSON.stringify(data))
+console.log(data[child])
 const updateName = () => {
     document.querySelector('#rekor-namaOrtu').innerText = `Nama Orang Tua: ${parent}`
     document.querySelector('#rekor-namaAnak').innerText =  `Nama Anak: ${child}`
@@ -10,12 +12,20 @@ const updateName = () => {
 window.onload = updateName()  //Instantly update name
 
 
+
 //Loop through the stressLvl array then print
 let rekorList = document.querySelector('.rekorList')
-for (const stressLvl of data[child]) { 
+// for (const stressLvl of localStorage.currLevel) { 
+//     let newLi = document.createElement('li')
+//     let newText = document.createTextNode(`${counter}. ${stressLvl}`)
+//     counter++
+//     newLi.appendChild(newText)
+//     rekorList.appendChild(newLi)
+// }
+
+for (let i = 0; i < data[child].length; i++) {
     let newLi = document.createElement('li')
-    let newText = document.createTextNode(`${counter}. ${stressLvl}`)
-    counter++
+    let newText = document.createTextNode(`${i + 1}. ${data[child][i]}`)
     newLi.appendChild(newText)
-    rekorList.appendChild(newLi)
+    rekorList.appendChild(newLi)   
 }
